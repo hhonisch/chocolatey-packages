2d1279fa27716a2eedbf49f5415da921f80448a7
Hartmut Honisch <Hartmut.Honisch@gmail.com>
2021-08-31 23:56:00 +0200

    Check for files in install dir

--- test/TestPackageInstall.bat
+++ test/TestPackageInstall.bat
@@ -17,6 +17,19 @@ echo.
 choco install help-workshop --source "%PACKAGE_SOURCE_DIR%" --yes --force --no-progress --params="'/InstallDir:%PACKAGE_INSTALL_DIR%'" || goto error
 
 echo.
+echo *** Test whether files exist in install dir
+echo.
+for %%I in (hcw.exe hcw.hlp hcrtf.exe dbhe.exe) do (
+  echo Looking for %%I...
+  if not exist "%PACKAGE_INSTALL_DIR%\%%I" (
+    echo File not found: %PACKAGE_INSTALL_DIR%\%%I
+    set ERROR_OCCURRED=1
+  ) else (
+    echo   ...found
+  )
+)
+
+echo.
 echo *** Uninstall chocolatey package ***
 echo.
 choco uninstall help-workshop --yes --force --no-progress" || goto error
@@ -34,7 +47,7 @@ echo.
 copy /Y "%ChocolateyInstall%\logs\chocolatey.log" "%MYDIR%\chocolatey.log"
 echo.
 echo ************************************************************
-if "%ERROR_OCCURRED%"=="1" (
+if not "%ERROR_OCCURRED%"=="" (
   echo *** %SCRIPT_TITLE%: ERROR^(s^) %DATE% %TIME:~0,-3%
   echo *** One or more errors occurred. Check the log.
 ) else (
